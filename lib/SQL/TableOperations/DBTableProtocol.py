d9ac28d8242a29a221c8501dfa097b7edf84f0ff
pscpeng <pscpeng@outlook.com>
2023-10-27 22:13:07 +0800

    Add type notations.

--- lib/SQL/TableOperations/DBTableProtocol.py
+++ lib/SQL/TableOperations/DBTableProtocol.py
@@ -1,14 +1,17 @@
 from abc import ABC, abstractmethod
 
+from ..DB import DB
+
 
 class DBTableProtocol(ABC):
-    def __init__(self, table_name):
+    def __init__(self, db_instance: DB, table_name: str):
+        self.db_instance = db_instance
         self.table_name = table_name
 
     @abstractmethod
-    def insert_record(self, insert_dict):
+    def insert_record(self, insert_dict: dict):
         pass
 
     @abstractmethod
-    def fetch_table(self, option):
+    def fetch_table(self, option: str):
         pass
--- lib/SQL/utils.py
+++ lib/SQL/utils.py
@@ -17,7 +17,7 @@ def get_table_from_cursor(cursor: cursors.Cursor):
     return table
 
 
-def generate_insert_sql_from_dict(table_name, insert_dict):
+def generate_insert_sql_from_dict(table_name: str, insert_dict: dict):
     from functools import reduce
     sql_key_value_pair = reduce(
         lambda old_pair, new_pair: (', '.join((old_pair[0], new_pair[0])), ', '.join((old_pair[1], new_pair[1]))),
