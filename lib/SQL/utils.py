bb4a92aba8498ada533cb8129e313ee2ab11cbb1
pscpeng <pscpeng@outlook.com>
2023-10-27 20:39:00 +0800

    Add function for converting dict to SQL INSERT command string.

--- lib/SQL/utils.py
+++ lib/SQL/utils.py
@@ -16,3 +16,14 @@ def get_table_from_cursor(cursor: cursors.Cursor):
     table = [column_names] + table_rows_list
 
     return table
+
+
+def generate_insert_sql_from_dict(table_name, insert_dict):
+    from functools import reduce
+    sql_key_value_pair = reduce(
+        lambda old_pair, new_pair: (', '.join((old_pair[0], new_pair[0])), ', '.join((old_pair[1], new_pair[1]))),
+        insert_dict.items())
+    sql_insert_string = 'INSERT INTO {name} ({keys}) VALUES ({values})'.format(name=table_name,
+                                                                               keys=sql_key_value_pair[0],
+                                                                               values=sql_key_value_pair[1])
+    return sql_insert_string
