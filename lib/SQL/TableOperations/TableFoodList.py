09848129def931bb5678862efced4a2120e26db4
pscpeng <pscpeng@outlook.com>
2023-10-28 18:03:16 +0800

    Add TableFoodList.

---
+++ lib/SQL/TableOperations/TableFoodList.py
@@ -0,0 +1,85 @@
+from lib.SQL.DB import DB
+from lib.SQL.TableOperations.DBTableProtocol import DBTableProtocol
+
+
+class TableFoodList(DBTableProtocol):
+    def __init__(self, db_instance: DB):
+        super().__init__(db_instance)
+
+    @property
+    def table_name(self):
+        return 'FoodList'
+
+    def insert_record(self, insert_dict: dict):
+        assert all((key in ['food_brand', 'food_name', 'food_category', 'food_unit']) for key in list(
+            insert_dict.keys())), \
+            'Find unsupported keys, only [food_brand, food_name, food_category, food_unit] are supported'
+
+        self.db_instance.insert_table_by_dict(self.table_name, insert_dict)
+
+    def fetch_record(self, for_key: int):
+        command = '''
+            SELECT *
+            FROM {table_name}
+            WHERE food_id = {id}
+        '''.format(table_name=self.table_name, id=for_key)
+        table = self.db_instance.fetch_table_by_command(command)
+        return table
+
+    # TODO: Add the delete and update functions.
+    def delete_record(self, for_key: int):
+        pass
+
+    def update_record(self, for_key: int, new_dict: dict):
+        pass
+
+    def add_food_record(self, brand: str, name: str, category: str, unit: str):
+        self.insert_record({'food_brand': brand,
+                            'food_name': name,
+                            'food_category': category,
+                            'food_unit': unit})
+
+    def get_food_record_by_id(self, food_id: int):
+        return self.fetch_record(food_id)
+
+    def find_food_record_by_name(self, name: str):
+        command = '''
+            SELECT *
+            FROM {table_name}
+             WHERE food_name LIKE \'%{name}%\'
+        '''.format(table_name=self.table_name, name=name)
+        return self.db_instance.fetch_table_by_command(command)
+
+    def get_food_list(self):
+        command = '''
+            SELECT *
+            FROM {table_name}
+        '''.format(table_name=self.table_name)
+        return self.db_instance.fetch_table_by_command(command)
+
+    def get_food_product_list(self):
+        command = '''
+            SELECT CONCAT_WS('-', food_brand, food_name) AS product_name
+            FROM {table_name}
+        '''.format(table_name=self.table_name)
+        return self.db_instance.fetch_table_by_command(command)
+
+
+def test():
+    db = DB.from_yaml('../../../db_info.yml')
+    table_food = TableFoodList(db)
+
+    table_food.insert_record({'food_brand': '百事', 'food_name': '可乐', 'food_category': '饮料', 'food_unit': '罐'})
+
+    result = table_food.get_food_record_by_id(15)
+    print(result)
+    search = table_food.find_food_record_by_name('猫条')
+    print(search)
+    all = table_food.get_food_list()
+    print(all)
+    products = table_food.get_food_product_list()
+    print(products)
+
+
+if __name__ == '__main__':
+    test()
