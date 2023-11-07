7a3bfbd846f4aa38b51f7f863bc06f7e074823aa
pscpeng <pscpeng@outlook.com>
2023-11-07 23:22:20 +0800

    Add router for stool records.

--- src/DataOperations/StoolRecords.py
+++ src/DataOperations/StoolRecords.py
@@ -1,8 +1,12 @@
 from datetime import datetime
+from pathlib import Path
 
+from fastapi import APIRouter
 from pydantic import BaseModel, validator
 
-from lib.SQL.TableOperations.TableStoolRecords import StoolStatus
+from lib.SQL.DB import DB
+from lib.SQL.TableOperations.TableStoolRecords import StoolStatus, TableStoolRecords
+from lib.Web.utils import table_to_dict
 
 
 class StoolRecordModel(BaseModel):
@@ -24,3 +28,44 @@ class StoolRecordModel(BaseModel):
             return input_status
         else:
             raise ValueError('Not a valid stool status')
+
+
+db = DB.from_yaml(Path('db_info.yml').resolve())  # Working directory is the project root
+table_stool = TableStoolRecords(db)
+
+stool_router = APIRouter(
+    prefix='/stool',
+    tags=['Stool Records']
+)
+
+
+@stool_router.post('/add_record')
+async def add_record(record: StoolRecordModel):
+    record_dict = record.dict()
+    try:
+        table_stool.insert_record(record_dict)
+    except Exception as error:
+        print(error)
+        message = {"message": str(error)}
+        return message
+    else:
+        status_dict = {"inserted": record_dict, "message": "ok"}
+        return status_dict
+
+
+@stool_router.get('/get_full_table')
+async def get_full_table():
+    table = table_stool.get_stool_records_all()
+    return table_to_dict(table)
+
+
+@stool_router.get('/get_records_by_date/{date}')
+async def get_stool_record_by_date(date: str):
+    table = table_stool.get_stool_records_by_date(date=date)
+    return table_to_dict(table)
+
+
+@stool_router.get('/get_records_by_interval/{date_start}/{date_end}')
+async def get_records_by_interval(date_start: str, date_end: str):
+    table = table_stool.get_stool_records_by_interval(date_start, date_end)
+    return table_to_dict(table)
