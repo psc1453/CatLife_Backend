c34011496014f4893bf6cd998882619c26ff2dd5
pscpeng <pscpeng@outlook.com>
2023-11-08 00:46:20 +0800

    Add router for diet summary view

--- src/DataOperations/DietRecords.py
+++ src/DataOperations/DietRecords.py
@@ -6,6 +6,7 @@ from pydantic import BaseModel, validator
 
 from lib.SQL.DB import DB
 from lib.SQL.TableOperations.TableDietRecords import TableDietRecords
+from lib.SQL.ViewOperations.ViewDietEveryday import ViewDietEveryday
 from lib.Web.utils import table_to_dict
 
 
@@ -35,6 +36,7 @@ class DietRecordModel(BaseModel):
 
 db = DB.from_yaml(Path('db_info.yml').resolve())  # Working directory is the project root
 table_diet = TableDietRecords(db)
+view_diet = ViewDietEveryday(db)
 
 diet_router = APIRouter(
     prefix='/diet',
@@ -78,3 +80,22 @@ async def get_diet_records_by_date(date: str):
 async def get_diet_records_by_interval(date_start: str, date_end: str):
     table = table_diet.get_diet_records_by_interval(date_start, date_end)
     return table_to_dict(table)
+
+
+###
+@diet_router.get('/summary/get_full_diet_summary')
+async def get_full_diet_summary():
+    view = view_diet.get_diet_records_all()
+    return table_to_dict(view)
+
+
+@diet_router.get('/summary/get_diet_summary_by_date/{date}')
+async def get_diet_summary_by_date(date: str):
+    view = view_diet.get_diet_records_by_date(date=date)
+    return table_to_dict(view)
+
+
+@diet_router.get('/summary/get_diet_summary_by_interval/{date_start}/{date_end}')
+async def get_diet_summary_by_interval(date_start: str, date_end: str):
+    view = view_diet.get_diet_records_by_interval(date_start, date_end)
+    return table_to_dict(view)
