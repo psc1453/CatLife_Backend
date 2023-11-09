0acef4cfacfe8d58ba4842ad40c02269ab868b57
pscpeng <pscpeng@outlook.com>
2023-11-10 00:33:00 +0800

    Fixed a bug for server environment.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import os
+
 import uvicorn
 
 if __name__ == "__main__":
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
     uvicorn.run('main_fastapi:CatLife_Backend', host="0.0.0.0", port=228, reload=True)
